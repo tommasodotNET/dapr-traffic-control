49d3500bc17c975600c93109a4ea32d5cf58372d
Tommaso Stocchi <tommasostocchi@outlook.com>
2022-06-27 16:58:08 +0200

    -fix maildev ports

--- src/Infrastructure/maildev/start-maildev.ps1
+++ src/Infrastructure/maildev/start-maildev.ps1
@@ -1,2 +1,2 @@
 # using older version of maildev (newer version did not render HTML mail correctly)
-docker run -d -p 4000:80 -p 4025:25 --name dtc-maildev maildev/maildev:latest
+docker run -d -p 4000:1080 -p 4025:1025 --name dtc-maildev maildev/maildev:latest
